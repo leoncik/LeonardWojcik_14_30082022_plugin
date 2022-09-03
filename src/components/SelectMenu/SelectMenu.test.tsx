858570954444a5f276e2e38687121b42d42dba12
Léonard Wojcik <leonardw@tutanota.com>
2022-09-03 13:41:31 +0200

    feat: setup test environment

---
+++ src/components/SelectMenu/SelectMenu.test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render, screen } from '@testing-library/react';
+
+import SelectMenu from "./SelectMenu";
+
+describe("Testing select menu", () => {
+  it("should render the component properly", () => {
+    render(<SelectMenu label="I am a test!" />);
+    const textLabel = screen.getByText('I am a test!');
+    expect(textLabel.innerHTML).toBe("I am a test!");
+  });
+});
