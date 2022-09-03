2208bfabdc3d51aa50b107c842eba1bcb4572623
Léonard Wojcik <leonardw@tutanota.com>
2022-09-03 15:07:41 +0200

    feat: add storybook

---
+++ src/components/SelectMenu/SelectMenu.stories.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import SelectMenu from "./SelectMenu";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "ReactComponentLibrary/SelectMenu",
+  component: SelectMenu,
+} as ComponentMeta<typeof SelectMenu>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof SelectMenu> = (args) => <SelectMenu {...args} />;
+
+export const HelloWorld = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+HelloWorld.args = {
+  label: "I am a test!",
+};
+
+export const ClickMe = Template.bind({});
+ClickMe.args = {
+  label: "Click me!",
+};
