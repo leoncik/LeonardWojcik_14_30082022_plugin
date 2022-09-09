7798c613132dc670bbc6642148ac28fec0d924b1
Léonard Wojcik <leonardw@tutanota.com>
2022-09-09 18:30:39 +0200

    build: change build settings - Fix bug (while trying to import library) caused by build settings.

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,26 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
+import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
+import { uglify } from "rollup-plugin-uglify";
+import { babel } from '@rollup/plugin-babel';
 
-const packageJson = require("./package.json");
-
-export default [
-  {
-    input: "src/index.ts",
+export default {
+    input: "./lib/index.ts",
     output: [
-      {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: "esm",
-        sourcemap: true,
-      },
+        {
+            file: "dist/index.js",
+            format: 'es'
+        }
     ],
     plugins: [
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-      postcss(),
-    ],
-  },
-  {
-    input: "dist/esm/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-    external: [/\.css$/],
-  },
-];
+        peerDepsExternal(),
+        resolve(),
+        typescript(),
+        postcss({
+            extensions: ['.css']
+        }),
+        uglify(),
+        babel()
+    ]
+}
