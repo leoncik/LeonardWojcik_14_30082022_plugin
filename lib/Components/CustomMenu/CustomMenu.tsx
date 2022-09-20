2fa9447548e054c4b3bf48b0f850fee5d92253e9
Léonard Wojcik <leonardw@tutanota.com>
2022-09-20 14:54:28 +0200

    fix: replace useLayoutEffect with useEffect - Replace hooks to avoid visual bugs on first render.

--- lib/Components/CustomMenu/CustomMenu.tsx
+++ lib/Components/CustomMenu/CustomMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // import './SelectMenu.css';
 
 export interface selectMenuProps {
@@ -30,7 +30,7 @@ const CustomMenu = ({
 }: selectMenuProps) => {
     // Get height of the custom button to set menu position
     const [customButtonHeight, setCustomButtonHeight] = useState(0);
-    useLayoutEffect(() => {
+    useEffect(() => {
         setCustomButtonHeight(customButtonRef.current.offsetHeight);
     }, []);
 
